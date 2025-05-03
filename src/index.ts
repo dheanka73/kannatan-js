4042b6984cac7ae9fd4984bead53da1dc329fd15
Dhean Kasanda <dheanka73.io@gmail.com>
2025-05-03 14:20:07 +0700

    Add basic bot code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hi")
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import "dotenv/config";
+
+const token = process.env.BOT_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token).then();
